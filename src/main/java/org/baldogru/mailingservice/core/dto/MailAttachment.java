f2608a3978cac69de17cebf8281595cf2282ab5e
Moler <moler555@gmail.com>
2018-02-17 11:04:47 +0100

    Mail-6 Użyłem buildera z lomboka, skoro już go używamy.

--- src/main/java/org/baldogru/mailingservice/core/dto/MailAttachment.java
+++ src/main/java/org/baldogru/mailingservice/core/dto/MailAttachment.java
@@ -1,7 +1,23 @@
 package org.baldogru.mailingservice.core.dto;
 
+
+import lombok.*;
+
+import java.io.File;
+import java.time.LocalDateTime;
+
 /**
  * Klasa zawierająca szczegóły załącznika mailowego.
  */
-public class MailAttachment {
+@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+@Builder
+@Getter
+public final class MailAttachment {
+    private final Long id;
+    private final String name;
+    private final String extension;
+    private final Long size;
+    private final LocalDateTime createDate;
+    private final File attachmentFile;
+
 }
