14eaee1c8eb7a7ecb80441b8b397ba11097adfc6
tomasz.wieclaw <tomash.oov@gmail.com>
2018-02-18 22:42:22 +0100

    Poprawka po konfliktach. Dodałem pliki .orig do gitignore

--- src/test/java/org/baldogru/mailingservice/core/service/MailAttachmentServiceImplTest.java
+++ src/test/java/org/baldogru/mailingservice/core/service/MailAttachmentServiceImplTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertNull;
 public class MailAttachmentServiceImplTest {
 
     @Mock
-    priavate MailAttachmentDTO mailAttachmentDTO;
+    private MailAttachmentDTO mailAttachmentDTO;
     @InjectMocks
     private MailAttachmentServiceImpl mailAttachmentService;
 
